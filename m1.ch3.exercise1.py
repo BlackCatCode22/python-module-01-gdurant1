8445028d6fc44c1d8dcba35e67f1db175a987a06
Durant <gdurant1@my.scccd.edu>
2025-08-17 11:12:52 -0700

    Add files via upload

---
+++ m1.ch3.exercise1.py
@@ -0,0 +1,10 @@
+try:
+    hours= float(input('Hours worked?'))
+    rate= float(input('Rate worked?'))
+except ValueError:
+    print('Error, please enter a number.')
+    exit()
+if (hours<=40):
+    print('Pay:', hours*rate)
+else:
+    print('Pay:', (40*rate)+((hours-40)*(rate*1.5)))
