d12097da6feefd51c18c3b4b9e8220f061c8a405
Durant <gdurant1@my.scccd.edu>
2025-08-23 22:29:59 -0700

    Add files via upload

---
+++ m1.ch1.exercise3.py
@@ -0,0 +1,4 @@
+print('What is the difference between a compiler and an interpreter?\n'
+      'An interpreter reads, translates and executes the program line by line while the program is running.\n'
+      'A compiler needs to translate the entire program file before running.\n'
+      'Compilers translates the entire program into machine level language and saves it as an executable for later use.')
