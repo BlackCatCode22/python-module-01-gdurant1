42c44e0ef111c01e05d3dac7b30c43e0f0c6a410
Durant <gdurant1@my.scccd.edu>
2025-08-23 23:05:46 -0700

    Add files via upload

---
+++ m1.ch2.exercise4.py
@@ -0,0 +1,10 @@
+print('Assume that we execute the following assignment statments:\n'
+      'width = 17\n'
+      'height = 12.0\n'
+      'For each of the following expressions, write value of the expression and the type.')
+width=17
+height=12.0
+print("value is:", width//2, "and type is:", type(width//2))
+print("value is:", width/2.0, "and type is:", type(width/2.0))
+print("value is:", height/3, "and type is:", type(height/3))
+print("value is:", 1+2*5, "and type is:", type(1+2*5))
---
+++ m1.ch2.exercise5.py
@@ -0,0 +1,3 @@
+Celsius = float(input('Celsius Temperature = '))
+
+print('Fahrenheit =', Celsius * 9/5 + 32 )
